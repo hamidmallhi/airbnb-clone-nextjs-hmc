1c250e4e2914194a069c9eabdb656aca343eb074
Hamid Mallhi <hamidmallhi@Hamids-MacBook-Pro.local>
2021-12-13 15:26:14 -0700

    day 2

--- components/Header.js
+++ components/Header.js
@@ -4,25 +4,72 @@ import {
     GlobeAltIcon,
     MenuIcon,
     UserCircleIcon,
-    usersIcon, 
+    UsersIcon, 
 } from '@heroicons/react/solid'
+import { useState } from 'react'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { useRouter } from "next/dist/client/router";
 
 
-function Header() {
+function Header({placeholder}) {
+
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [searchInput, setSearchInput] = useState("");
+    const [noOfGuests, setNoOfGuests] = useState(1);
+    const router = useRouter();
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection",
+      };
+
+      function handleSelect(ranges) {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+      }
+      function resetInput() {
+        setSearchInput("");
+      }
+      function search() {
+        if (!searchInput) return;
+        // to push the location, start date, end date and no-of-guests variables in to url through the query parameters and in the search page we will grab those variables
+        router.push({
+          pathname: "/search",
+          query: {
+            location: searchInput,
+            startDate: startDate.toISOString()   ,
+            endDate: endDate.toISOString(),
+            noOfGuests,
+          },
+        });
+    
+        setSearchInput("");
+      }
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-            <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+            <div 
+                onClick={() => router.push("/")}
+                className='relative flex items-center h-10 cursor-pointer my-auto'>
                 <Image src="https://links.papareact.com/qd3"
                     layout="fill"
                     objectFit='contain'
                     objectPosition='left'
+                    priority
                 />
             </div>
             <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm '>
-                <input className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400"
-                type="text"
-                placeholder="Start your search"
-                />
+                <input 
+                    value={searchInput}
+                    onChange={e => setSearchInput(e.target.value)}
+                    className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400"
+                    type="text"
+                    placeholder={ placeholder || 'Start your search' }
+                    />
                 <SearchIcon className="hidden md:inline-flex mx-2 h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer "/>
             </div>
             <div className='flex items-center space-x-4 justify-end text-gray-500'>
@@ -33,6 +80,37 @@ function Header() {
                     <UserCircleIcon className='h-6'/>
                 </div>
             </div>
+            {searchInput && (
+                <div className="flex flex-col col-span-3 mx-auto mt-0">
+                    <DateRangePicker
+                        minDate={new Date()}
+                        rangeColors={["#FD5B61"]}
+                        ranges={[selectionRange]}
+                        onChange={handleSelect}
+                    />
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="text-2xl flex-grow font-semibold">
+                            Number of Guests
+                        </h2>
+                        <UsersIcon className="h-5" />
+                        <input
+                        className="w-12 pl-2 text-lg outline-none text-red-400"
+                        type="number"
+                        min={1}
+                        value={noOfGuests}
+                        onChange={(e) => setNoOfGuests(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex">
+                        <button onClick={resetInput} className="flex-grow text-gray-500">
+                        Cancel
+                        </button>
+                        <button onClick={search} className="flex-grow text-red-400">
+                        Search
+                        </button>
+                    </div>
+                </div>
+            )}
         </header>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,6 @@ module.exports = {
     extend: {},
   },
   plugins: [require("tailwind-scrollbar-hide")],
+  
 };
 
